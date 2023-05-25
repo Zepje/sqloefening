f0e01e1263765fff02b9cae761a01f14a7c1907b
Zepje <seppe.landtsheer@gmail.com>
2023-05-25 17:33:11 +0200

    final push, nog een paar kleine changes, form 1 is grotendeels klaar

--- sqloefening/Form1.Designer.cs
+++ sqloefening/Form1.Designer.cs
@@ -28,12 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.inschrijvingenDataGridView = new System.Windows.Forms.DataGridView();
+            this.workshopToevoegenButton = new System.Windows.Forms.Button();
+            this.workshopAanmeldenButton = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.inschrijvingenDataGridView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // inschrijvingenDataGridView
+            // 
+            this.inschrijvingenDataGridView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.inschrijvingenDataGridView.Location = new System.Drawing.Point(12, 12);
+            this.inschrijvingenDataGridView.Name = "inschrijvingenDataGridView";
+            this.inschrijvingenDataGridView.RowHeadersWidth = 51;
+            this.inschrijvingenDataGridView.RowTemplate.Height = 29;
+            this.inschrijvingenDataGridView.Size = new System.Drawing.Size(776, 339);
+            this.inschrijvingenDataGridView.TabIndex = 0;
+            // 
+            // workshopToevoegenButton
+            // 
+            this.workshopToevoegenButton.Location = new System.Drawing.Point(12, 369);
+            this.workshopToevoegenButton.Name = "workshopToevoegenButton";
+            this.workshopToevoegenButton.Size = new System.Drawing.Size(167, 53);
+            this.workshopToevoegenButton.TabIndex = 1;
+            this.workshopToevoegenButton.Text = "Voeg Workshop Toe";
+            this.workshopToevoegenButton.UseVisualStyleBackColor = true;
+            this.workshopToevoegenButton.Click += new System.EventHandler(this.workshopToevoegenButton_Click);
+            // 
+            // workshopAanmeldenButton
+            // 
+            this.workshopAanmeldenButton.Location = new System.Drawing.Point(185, 369);
+            this.workshopAanmeldenButton.Name = "workshopAanmeldenButton";
+            this.workshopAanmeldenButton.Size = new System.Drawing.Size(198, 53);
+            this.workshopAanmeldenButton.TabIndex = 2;
+            this.workshopAanmeldenButton.Text = "Schrijf Student In Voor Workshop";
+            this.workshopAanmeldenButton.UseVisualStyleBackColor = true;
+            this.workshopAanmeldenButton.Click += new System.EventHandler(this.workshopAanmeldenButton_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.workshopAanmeldenButton);
+            this.Controls.Add(this.workshopToevoegenButton);
+            this.Controls.Add(this.inschrijvingenDataGridView);
+            this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.inschrijvingenDataGridView)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DataGridView inschrijvingenDataGridView;
+        private Button workshopToevoegenButton;
+        private Button workshopAanmeldenButton;
     }
 }
--- sqloefening/Program.cs
+++ sqloefening/Program.cs
@@ -11,7 +11,7 @@ namespace sqloefening
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            Application.Run(new WorkshopToevoegen());
+            Application.Run(new Form1());
         }
     }
 }
